85b4a9bf48331af1c9f6e430f9f73dbb6bc48a63
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-10-30 18:10:23 +0530

    let's add the rest of the features after coming back from gym. now for me to make anything good enough, i have to learn the techniques, lol. it's so dumb thinking that you made the tool before learning the technique.

--- shader.glsl
+++ shader.glsl
@@ -14,6 +14,10 @@ vec4 effect(vec4 color, Image tex, vec2 tex_coords, vec2 sc) {
   vec2 mouse = (iMouse.xy / iResolution) * 2.0 - 1.0;
   mouse *= vec2(AR, -1);
 
+  uv.x = sin(uv.x + iTime);
+  uv.y += 1.25;
+
+
   vec3 col = vec3(uv.x, 0.0, uv.y);
   return vec4(col, 1.0);
 }

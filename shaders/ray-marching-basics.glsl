0b3b227052c85ac96f4e87f04210145ba0d472a7
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-11-08 00:54:23 +0530

    well, started a new basics file. imma do something good today.

--- shaders/ray-marching-basics.glsl
+++ shaders/ray-marching-basics.glsl
@@ -22,10 +22,42 @@ uniform vec3 iResolution;
 // HELPERS
 vec3 rgb(float r, float g, float b) { return vec3(r, g, b) / 255; }
 
+float sdfSphere(in vec3 p, in vec3 c, float r) { return length(p - c) - r; }
+
 // MAP
+float map(in vec3 pos) { return sdfSphere(pos, vec3(0, 0, -2), 1.0); }
 
 // RAY MARCHER
-vec3 march_ray(in vec3 ro, in vec3 rd) { return vec3(1.0, 0, 0); }
+vec3 march_ray(in vec3 ro, in vec3 rd) {
+  // total distance travelled
+  float t = 0.0;
+  float NUM_MAX_STEPS = 256;
+  float MIN_HIT_DISTANCE = 0.001;
+  float MAX_TRACE_DISTANCE = 1000.0;
+
+  for (int i = 0; i < NUM_MAX_STEPS; i += 1) {
+    // current position
+    vec3 curr_pos = ro + rd * t;
+
+    // distance to closest
+    float d = map(curr_pos);
+
+    if (d <= MIN_HIT_DISTANCE) {
+      // do calculations adn return color
+      return vec3(1, 0, 0);
+    }
+
+    if (t > MAX_TRACE_DISTANCE) {
+      break;
+    }
+
+    // updating total distance travelled
+    t += d;
+  }
+
+  // background color
+  return rgb(255, 255, 255);
+}
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
   // aspect ratio
